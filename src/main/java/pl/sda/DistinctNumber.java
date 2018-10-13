5a61f34ed56492966de63bab7bf6f17ffd340e89
Zdzislaw Trzaska <ztrzaska2@gmail.com>
2018-10-13 14:47:05 +0200

    zadanie dodatkowe, liczności wystąpień

--- src/main/java/pl/sda/DistinctNumber.java
+++ src/main/java/pl/sda/DistinctNumber.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
 
 public class DistinctNumber {
 
@@ -24,19 +26,40 @@ public class DistinctNumber {
             }
 
             checkDistinctCount(cardinalities);
+            checkMostFrequentlyNumber(cardinalities);
         } catch (FileNotFoundException ex) {
             System.out.println("Exception with reading file " + ex);
         }
     }
 
+    private static void checkMostFrequentlyNumber(int[] cardinalities) {
+        int maxCardinality = 0;
+        int result = 0;
+        for (int i = 1; i < cardinalities.length; i++) {
+            if (cardinalities[i] > maxCardinality) {
+                result = i;
+                maxCardinality = cardinalities[i];
+            }
+        }
+        System.out.println("Najczęściej wystąpiła liczba " + result);
+
+    }
+
     private static void checkDistinctCount(int[] cardinalities) {
         int result = 0;
-        for (int i = 1; i <= 10; i++) {
+        for (int i = 1; i < cardinalities.length; i++) {
             if (cardinalities[i] != 0) {
                 result++;
             }
         }
         System.out.println("Wystąpiło " + result + " różnych liczb ");
+
+
+/*      //druga opcja, rozwiązanie z wykorzystaniem stream-u
+        long res2 = IntStream.range(1, cardinalities.length)
+                .filter(t -> cardinalities[t] != 0).count();
+        System.out.println("Wystąpiło " + res2 + " różnych liczb ");*/
+
     }
 
 
